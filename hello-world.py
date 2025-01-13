41e532435fa2762fef16453cc34341382d13b9f4
gangach <chandran_gangadharan@manulife.com>
2025-01-13 13:59:47 -0500

    added pandas changes

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
 print("hello world")
+# Sample DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]}
+df = pd.DataFrame(data)
+
+# Select a value from an index in the DataFrame
+value = df.loc[1, 'Name']
+print(value)
